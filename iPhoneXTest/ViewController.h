fb130dc818263fa4303db4da523837d90bf77351
黄珂耀 <495751349@qq.com>
2017-11-29 10:49:44 +0800

    Initial Commit

---
+++ iPhoneXTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iPhoneXTest
+//
+//  Created by 黄珂耀 on 2017/11/29.
+//  Copyright © 2017年 黄珂耀. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iPhoneXTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iPhoneXTest
+//
+//  Created by 黄珂耀 on 2017/11/29.
+//  Copyright © 2017年 黄珂耀. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
